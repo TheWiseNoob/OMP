d4e331d7b1ba3f62e84d3a0c07e1b0394d423b2a
DJ Griffin <thewisenoob@gmail.com>
2018-11-23 21:29:14 -0500

    Icon location wrong fix.

--- GUI/GUI.cc
+++ GUI/GUI.cc
@@ -1,41 +1,3 @@
-/* ////////////////////////////////////////////////////////////////////////////   
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//  The developer(s) of the OMP audio player hereby grant(s) permission
-//  for non-GPL compatible GStreamer plugins to be used and distributed
-//  together with GStreamer and OMP. This permission is above and beyond
-//  the permissions granted by the GPL license by which OMP is covered.
-//  If you modify this code, you may extend this exception to your version
-//  of the code, but you are not obligated to do so. If you do not wish to do
-//  so, delete this exception statement from your version.
-//
-//  Libraries used by OMP:
-//
-//    - clastfm: http://liblastfm.sourceforge.net/ 
-//
-//    - gstreamer: https://gstreamer.freedesktop.org/ 
-//
-//    - gtkmm: https://www.gtkmm.org/en/
-//
-//    - libconfig: http://www.hyperrealm.com/libconfig/
-//
-//    - standard C and C++ libraries
-//
-//    - taglib: http://taglib.org/
-//
-//////////////////////////////////////////////////////////////////////////// */
 
 
 
@@ -154,6 +116,8 @@
 #include <gtkmm/frame.h>
 
 #include <gtkmm/headerbar.h>
+
+#include <gtkmm/icontheme.h>
 
 #include <gtkmm/label.h>
 
@@ -364,9 +328,9 @@ GUI::GUI(Base& base_ref)
 
   // 
   vector<Glib::RefPtr<Gdk::Pixbuf>> icon_pixbufs;
-
+/*
   // 
-  string icon_16p_filename = "/usr/share/OMP/OMP_Icon_16.png";
+  string icon_16p_filename = "/usr/share/icons/hicolor/16.png";
 
   // 
   Glib::RefPtr<Gdk::Pixbuf> icon_16p_pixbuf
@@ -404,9 +368,9 @@ GUI::GUI(Base& base_ref)
 
   // 
   icon_pixbufs . push_back(icon_64p_pixbuf);
-
+*/
   // 
-  string icon_128p_filename = "/usr/share/OMP/OMP_Icon_128.png";
+  string icon_128p_filename = "/usr/share/icons/hicolor/128x128/apps/omp.png";
 
   // 
   Glib::RefPtr<Gdk::Pixbuf> icon_128p_pixbuf
