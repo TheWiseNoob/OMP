60f0ca3947d7378a2cf66c087221d99859ab2ef0
DJ Griffin <dj@djgriffinweb.dev>
2023-10-23 20:27:03 -0400

    Switch to pure GTK4, not gtkmm4.

--- Main.cc
+++ Main.cc
@@ -1,101 +1,43 @@
-/* ////////////////////////////////////////////////////////////////////////////   
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//  The developer(s) of the OMP audio player hereby grant(s) permission
-//  for non-GPL compatible GStreamer plugins to be used and distributed
-//  together with GStreamer and OMP. This permission is above and beyond
-//  the permissions granted by the GPL license by which OMP is covered.
-//  If you modify this code, you may extend this exception to your version
-//  of the code, but you are not obligated to do so. If you do not wish to do
-//  so, delete this exception statement from your version.
-//
-//  Libraries used by OMP:
-//
-//    - clastfm: http://liblastfm.sourceforge.net/ 
-//
-//    - gstreamer: https://gstreamer.freedesktop.org/ 
-//
-//    - gtkmm: https://www.gtkmm.org/en/
-//
-//    - libconfig: http://www.hyperrealm.com/libconfig/
-//
-//    - standard C and C++ libraries
-//
-//    - taglib: http://taglib.org/
-//
-//////////////////////////////////////////////////////////////////////////// */
+#include <gtk/gtk.h>
 
-
-
-
-
-//         //
-//         //
-//         //
-// Headers ////////////////////////////////////////////////////////////////////
-//         //
-//         //
-//         //
-
-//                 //
-//                 //
-// Program Headers ////////////////////////////////////////////////////////////
-//                 //
-//                 //
-
-#include "Base.h"
-
-
-
-//                 //
-//                 //
-// Outside Headers ////////////////////////////////////////////////////////////
-//                 //
-//                 //
-
-#include <gtkmm/application.h>
-
-#include <gtkmm/window.h>
-
-
-
-
-
-
-//      //
-//      //
-//      //
-// Main ///////////////////////////////////////////////////////////////////////
-//      //
-//      //
-//      //
-
-class MyWindow : public Gtk::Window
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
 {
-  public:
-    MyWindow();
-};
-
-MyWindow::MyWindow()
-{
-  set_title("OMP");
-  set_default_size(200, 200);
+  g_print ("Hello World\n");
 }
 
-int main (int argc, char *argv[])
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
 {
-  auto app = Gtk::Application::create("com.openmusicplayer.OMP");
-  return app->make_window_and_run<MyWindow>(argc, argv);
+  GtkWidget *window;
+  GtkWidget *button;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  button = gtk_button_new_with_label ("Hello World");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  gtk_window_set_child (GTK_WINDOW (window), button);
+
+  gtk_window_present (GTK_WINDOW (window));
+  GtkSettings *settings = gtk_settings_get_default();
+  g_object_set(settings, "gtk_application_prefer_dark_theme", TRUE, NULL);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
